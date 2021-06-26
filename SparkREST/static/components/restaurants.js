56d34b0bed5a384e6b0395ec583816cc6e4db4b2
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2021-06-26 13:07:19 +0200

    stil

--- SparkREST/static/components/restaurants.js
+++ SparkREST/static/components/restaurants.js
@@ -6,22 +6,26 @@ Vue.component("restaurants", {
 	},
 	template: `
 	<div>
-		<h3>Prikaz restorana</h3>
-		<table border="1">
-			<tr bgcolor="lightgrey">
-				<th>Logo</th>
-				<th>Naziv</th>
-				<th>Tip</th>
-				<th>Status</th>
-				<th>Lokacija</th>
-			</tr>
-			<tr v-for="r in restaurants">
-				<td></td>
-				<td>{{r.name}}</td>
-				<td>{{r.type}}</td>
-				<td>{{r.status}}</td>
-				<td>{{r.location}}</td>
-			</tr>
+		<h3>Restorani:</h3>
+		<table class="styled-table" border="1">
+			<thead>
+				<tr bgcolor="lightgrey">
+					<th>Logo</th>
+					<th>Naziv</th>
+					<th>Tip</th>
+					<th>Status</th>
+					<th>Lokacija</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="r in restaurants">
+					<td></td>
+					<td>{{r.name}}</td>
+					<td>{{r.type}}</td>
+					<td>{{r.status}}</td>
+					<td>{{r.location}}</td>
+				</tr>
+			</tbody>
 		</table>
 	</div>
 	`,
